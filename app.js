f3092bac635cf8d36f45b4ee29e576519b07113c
Steven-Roduta <stevenroduta00@gmail.com>
2025-02-22 16:17:48 +0100

    add express js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const cors = require ("cors")
+const cors = require("cors")
 const express = require('express')
 const app = express()
 const port = 3000
@@ -6,9 +6,9 @@ const myRout = require('./router/postsRouter')
 const data = require('./data/data')
 
 //definiamo la middleware
+app.use(cors());
 app.use(express.static('public'));
 app.use('/post', myRout)
-app.use(cors())
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,12 +1,11 @@
 const validation = (req, res, next) => {
-    req.params.id = Number(req.params.id);
-  
-    if (isNaN(req.params.id)) {
-      return res.status(400).json({ error: "L'ID deve essere un numero valido." });
-    }
-  
-    next();
-  };
-  
-  module.exports = validation;
-  
+  req.params.id = Number(req.params.id);
+
+  if (isNaN(req.params.id)) {
+    return res.status(400).json({ error: "L'ID deve essere un numero valido." });
+  }
+
+  next();
+};
+
+module.exports = validation;
